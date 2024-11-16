4fc3c1c7451a64f8106f14e9623bbb9426d629ef
Bisworanjan34 <bisworanjan34@gmail.com>
2024-11-17 02:52:02 +0530

    add some animation

---
+++ mobile-call-animation/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Mobile from './components/mobile/Mobile'
+
+function App() {
+ 
+
+  return (
+  <div className="app">
+    <Mobile/>
+  </div>
+  )
+}
+
+export default App
---
+++ mobile-call-animation/src/components/mobile/Mobile.jsx
@@ -0,0 +1,126 @@
+import React, { useRef, useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Mobile.css"
+import { faCirclePause, faEllipsisVertical, faHandHolding, faKeyboard, faMicrophoneSlash, faPause, faPhone, faPhoneFlip, faShieldHalved, faVideo, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import { faRecordVinyl } from '@fortawesome/free-solid-svg-icons/faRecordVinyl';
+const Mobile = () => {
+    let [call,setCall]=useState(false)
+    let box1ref=useRef()
+
+    let morefun=()=>{
+        box1ref.current.classList.toggle('active')
+    }
+
+    let pickupfun=()=>{
+        setCall(true)
+    }
+  return (
+    <div>
+      {/* <h3>mobile-call-animation</h3> */}
+      <div className="container">
+
+     {
+        call ? 
+        <div className="sec-cont">
+        <div className="user">
+           <div className="img"></div>
+           <p className='sim-t'>Sim-1</p>
+            <h5>SHREE RAM</h5>
+            {/* <p>time</p> */}
+        </div>
+         <div className="call-div " ref={box1ref}>
+            <div className="main-box1">
+            <div className="box1">
+                    <div className="su subbox-1">
+                        <div className="circle">
+                        <FontAwesomeIcon icon={faCirclePause} size="" />
+                        </div>
+                        <p>Hold</p>
+                    </div>
+                    <div className="su subbox-2">
+                          <div className="circle">
+                          <FontAwesomeIcon icon={faRecordVinyl} size="" />
+                          </div>
+                        <p>Record</p>
+                    </div>
+                    <div className="su subbox-3">
+                          <div className="circle">
+                          <FontAwesomeIcon icon={faVideo} size="" />
+                          </div>
+                        <p>Video call</p>
+                    </div>
+                    <div className="su subbox-4">
+                          <div className="circle">
+                          <FontAwesomeIcon icon={faPhone} size="" />
+                          </div>
+                        <p>Add call</p>
+                    </div>
+            </div>
+            </div>
+            <div className="main-box2">
+            <div className="box2">
+                    <div className="su subbox-5">
+                          <div className="circle">
+                          <FontAwesomeIcon icon={faKeyboard} size="" />
+                          </div>
+                        <p>Keypad</p>
+                    </div>
+                    <div className="su subbox-6">
+                          <div className="circle">
+                          <FontAwesomeIcon icon={faMicrophoneSlash} size="" />
+                          </div>
+                        <p>Mute</p>
+                    </div>
+                    <div className="su subbox-7">
+                          <div className="circle">
+                          <FontAwesomeIcon icon={faVolumeHigh} size="" />
+                          </div>
+                        <p>speaker</p>
+                    </div>
+                    <div className="su subbox-8">
+                          <div className="circle" onClick={morefun}>
+                          <FontAwesomeIcon icon={faEllipsisVertical} size="" />
+                          </div>
+                        <p>More</p>
+                    </div>
+            </div>
+             <div className="end-call">
+                <div className="call-box" onClick={()=>setCall(false)}>
+                <FontAwesomeIcon icon={faPhoneFlip} size="" />
+                </div>
+             </div>
+            </div>
+         </div>
+      </div>:
+    // first cont show call false 
+      <div className="first-cont">
+          <div className="f-box1">
+          <div className="div">
+          <p className='verify'>
+            <FontAwesomeIcon icon={faShieldHalved}className='v-icon' />
+            Verified-called from</p>
+            <h3>jay shree ram</h3>
+            <div className="img2"></div>
+          </div>
+          </div>
+          <div className="f-box2">
+            <div className="div">
+                <p>Click to answer</p>
+                <div className="pickup-call" onClick={pickupfun}>
+                    <FontAwesomeIcon icon={faPhoneFlip} size='lg'/>
+                </div>
+                <p>Thanks for calling</p>
+            </div>
+          </div>
+      </div>
+     }
+
+      </div>    {/* container */}
+
+
+
+    </div>
+  )
+}
+
+export default Mobile
