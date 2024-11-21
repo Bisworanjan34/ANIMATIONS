e7d12946e92a2460533037cc64897beddbdb6908
Bisworanjan34 <bisworanjan34@gmail.com>
2024-11-21 17:17:45 +0530

    litilbit changes

--- payment-successful/src/components/Home.jsx
+++ payment-successful/src/components/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
     <div>
       <h5 className='font-bold text-2xl m-3 uppercase font-mono'>Payment animation</h5>
      <p className='text-green-500 ms-3'>online payment animation </p>
-     <div className="div gap-6 mt-10 flex ms-12">
+     <div className="div  mt-10  sm:flex-col ms-1">
 
-      <button className='bg-green-600 text-white h-[50px] w-[180px] px-2 rounded-md active:bg-green-700 border-0 ' onClick={()=>setpayment(true)}>payment-success</button>
+      <button className='bg-green-600 text-white h-[50px] w-[180px] px-2 rounded-md active:bg-green-700 border-0 mb-3' onClick={()=>setpayment(true)}>payment-success</button>
        <div className=" mt-[-23px]">
        {
             payment?<Payment setPayment={setpayment}/>:''
